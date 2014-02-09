661354412a7b932d65113e5e8de559ce3fb466d3
EC2 Default User <ec2-user@ip-172-31-32-153.ec2.internal>
2014-02-09 07:34:33 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Noroshi::Application.config.secret_key_base = '53daab0ff9ef2a83ef56d36df387a749df30165c13f59b24b25d0ee42745ce46dfd3c6aa6badc4f60cd76e80f41a7538d062e5829b6b6d2256cae67075e3fdd5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Noroshi::Application.config.session_store :cookie_store, key: '_noroshi_session'
