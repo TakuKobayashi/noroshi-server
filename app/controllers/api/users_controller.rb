30e1f4d581332d91bbc038952008ac1b30630e4f
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2015-01-24 16:53:32 +0900

    userのidも返すように修正

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,6 +2,10 @@
 class Api::UsersController < Api::BaseController
   skip_before_filter :authentication
 
+  def index
+    @user = MinagoraswitchUser.find_by!(auth_token: params[:auth_token])
+  end
+
   def create
     User.transaction do
       @user = MinagoraswitchUser.new(auth_token: SecureRandom.hex, type: "MinagoraswitchUser")
